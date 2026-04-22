bb2a4eed49aa7902fc9d78a11bc9dd3c3b1ac044
Dylan Richardson <richadsondylan106@gmail.com>
2026-04-22 14:13:07 -0500

    Data-types header and source implementation 	-VERSION 0.1 	-Parts of code have been changed in retrospect for future development. 		-For example, Node has two distinct children instead of a vector of children. 		-AbstractSyntaxTree has excluded the addition of an "addChild" method 	-These data types may change in the future as directed for ease of development.

--- lib/data-types.h
+++ lib/data-types.h
@@ -1,29 +1,69 @@
 /*
-    Authors:
-    Description:
-    
+    Authors: Dylan Richardson
+    Description: Header file declaring important structs/classes for project.
+    VERSION: v0.1
+
+    REMINDER: This is an early development build. This may change, as the functionality
+    or requirements change to better suit the development of the parser and lexer.
 
 */
 #pragma once
+#include <string>
 
 
-
-struct Token{
-
-
+// Token Types will be defined as an enum.
+enum Operations { NUMBER, 
+     ADDOP,
+     SUBOP,
+     MULOP,
+     DIVOP,
+     EXPOP,
+     UNARYOP,
+     L_PAREN,
+     R_PAREN
 };
 
+
+// Struct can contained within header file as no methods are required.
+struct Token{
+    enum Operations type;
+
+    // Numbers are considered strings until checked later in parsing.
+    std::string value;
+    int position;
+};
+
+
 class Node{
+    private:
+	
+	Token value;
+
+	// children split into two distinct children.
+    	Node *leftChild;
+	Node *rightChild;
     
+    public:
+	Node(Token value);
+	Node(Token value, Node *leftChild);
+	Node(Token value, Node *leftChild, Node *rightChild);
+
+	Node *getLeftChild() const;
+	Node *getRightChild() const;
+	void pushLeftChild(Node *child);
+	void pushRightChild(Node *child);
 };
 
 class AbstractSyntaxTree{
     public:
-
+	Node *getRoot() const;
+	void setRoot(Node *newRoot);
+	// addChild method excluded for, as I am unsure how it would be
+	// implemented besides just using the Node class to add children.
 
 
     private:
-
+	Node *root;
     
 
 };
--- src/data-types.cpp
+++ src/data-types.cpp
@@ -1,6 +1,56 @@
 /*
-    Authors:
-    Description:
+    Authors: Dylan Richardson
+    Description: Implementation of data types used in the evaluator.
     
 
 */
+
+#include "../lib/data-types.h"
+
+
+
+// Node class implementation:
+Node::Node(Token value){
+    this->value = value;
+}
+
+Node::Node(Token value, Node *leftChild){
+    this->value = value;
+    this->leftChild = leftChild;
+}
+
+Node::Node(Token value, Node *leftChild, Node *rightChild){
+    this->value = value;
+    this->leftChild = leftChild;
+    this->rightChild = rightChild;
+}
+
+Node* Node::getLeftChild() const{
+    return this->leftChild;
+}
+
+Node* Node::getRightChild() const{
+    return this->rightChild;
+}
+
+void Node::pushLeftChild(Node *child){
+    this->leftChild = child;
+}
+
+void Node::pushRightChild(Node *child){
+    this->rightChild = child;
+}
+
+// End Node Class implementation
+// Begin AbstractSyntaxTree...
+
+Node* AbstractSyntaxTree::getRoot() const{
+    return this->root;
+}
+
+void AbstractSyntaxTree::setRoot(Node *newRoot){
+    this->root = newRoot;
+}
+
+
+
