eee944a0dea1b20aecd9383788b22e73f154f877
Kevin <kevinreilly25@gmail.com>
2026-04-30 07:26:50 -0500

    Updated parse-layer.h with helper functions used to recursively build the AST.
    Implemented parse-layer.cpp functions except for eval(Token*).
    
    eval() - Have a question about this function since it's said that the parse-layer is suppose to build the AST and then send it to the evaluation-layer I'm unsure what it's suppose to be evaluating.

--- lib/parse-layer.h
+++ lib/parse-layer.h
@@ -1,5 +1,5 @@
 /*
-    Authors:
+    Authors:Kevin Reilly
     Description:
     
     This is the parse layer of the expression evaluator. It is responsible for using
@@ -19,5 +19,12 @@ class ParseLayer : public EvaluationLayer{
 
     AbstractSyntaxTree AST;
 
-    void buildAST(Token*);  
+    void buildAST(Token*); 
+    
+    //Helper functions for building the AST
+    Node* parseExpression(Token* tokens, int& index);
+    Node* parseTerm(Token* tokens, int& index);
+    Node* parsePower(Token* tokens, int& index);
+    Node* parseUnary(Token* tokens, int& index);
+    Node* parsePrimary(Token* tokens, int& index);
 };
--- src/parse-layer.cpp
+++ src/parse-layer.cpp
@@ -1,6 +1,91 @@
+#include "parse-layer.h"
+#include "../lib/error.h"
+#include <string>
 /*
-    Author:
-    Description:
-    
-
+    Author: Kevin Reilly
+    Description: Recursively builds an Abstract Syntax Tree from an array of Tokens. 
 */
+
+double ParseLayer::eval(Token* tokens){
+
+}
+
+void ParseLayer::buildAST(Token* tokens) {
+    int index = 0; 
+    Node* root = parseExpression(tokens, index);
+    AST.setRoot(root);
+}
+
+Node* ParseLayer::parseExpression(Token* tokens, int& index) {
+    Node* left = parseTerm(tokens, index);
+
+    while (tokens[index].type == ADDOP || tokens[index].type == SUBOP) {
+        Token opToken = tokens[index];
+        index++;
+
+        Node* right = parseTerm(tokens, index);
+
+        left = new Node(opToken, left, right);
+    }
+
+    return left;
+}
+
+Node* ParseLayer::parseTerm(Token* tokens, int& index) {
+    Node* left = parsePower(tokens, index);
+
+    while (tokens[index].type == MULOP || tokens[index].type == DIVOP){
+        Token opToken = tokens[index];
+        index++;
+
+        Node* right = parsePower(tokens, index);
+        left = new Node(opToken, left, right);
+    }
+
+    return left;
+}
+
+Node* ParseLayer::parsePower(Token* tokens, int& index) {
+    Node* left = parseUnary(tokens, index);
+
+    if(tokens[index].type == EXPOP) {
+        Token opToken = tokens[index];
+        index++;
+        Node* right = parsePower(tokens, index);
+        left = new Node(opToken, left, right);
+    }
+
+    return left;
+}
+
+Node* ParseLayer::parseUnary(Token* tokens, int& index) {
+    if(tokens[index].type == UNARYOP){
+        Token opToken = tokens[index];
+        index++;
+        Node* child = parseUnary(tokens, index);
+
+        return new Node(opToken, child);
+    }
+
+    return parsePrimary(tokens, index);
+}
+
+Node* ParseLayer::parsePrimary(Token* tokens, int& index) {
+    if(tokens[index].type == NUMBER) {
+        Token number = tokens[index];
+        index++;
+        return new Node(number);
+    }
+
+    if (tokens[index].type == L_PAREN) {
+        index++;
+        Node* insideParens = parseExpression(tokens, index);
+
+        if (tokens[index].type != R_PAREN) {
+            throw Error(ODDPAREN, "Odd number of parentheses");
+        }
+        index++;
+        return insideParens;
+    }
+    throw Error(MISSOPERAND, "Expected a number or '('");
+}
